19cd40001ae6153e71d505381fa94762391183d1
Tanvir Ahammed <hmhridoy65@gmail.com>
2021-04-13 19:48:43 +0600

    Second Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,25 @@
 
-$('.owl-carousel').owlCarousel({
+$('.trail').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    navText:['<','>'],
+    autoplay: true,
+    autoplayTimeout:4000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+$('.topp').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
